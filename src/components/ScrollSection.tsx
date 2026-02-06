0afe65219747089aacecb2eb275985a9265bde28
Mario2902 <marioreyes55@hotmail.com>
2026-02-05 23:45:44 -0500

    fix bug in project mobile view

--- src/components/ScrollSection.tsx
+++ src/components/ScrollSection.tsx
@@ -1,32 +1,51 @@
 import { useActiveSection } from "@/context/ActiveSection";
 import { motion, useAnimation } from "framer-motion";
-import { type ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
 
 interface ScrollSectionProps {
     id: "home" | "about" | "projects" | "contact"
-    children: ReactNode
+    children: ReactNode,
+    animate?: boolean
 }
 
-export function ScrollSection({ id, children }: ScrollSectionProps) {
+export function ScrollSection({ id, children}: ScrollSectionProps) {
     const controls = useAnimation()
     const { setActiveSection } = useActiveSection()
+
+    const isMobile =
+        typeof window !== "undefined" && window.innerWidth < 768
+
+    useEffect(() => {
+        if (isMobile) {
+            controls.set({ opacity: 1, y: 0 })
+        } else {
+            controls.set({ opacity: 0, y: 60 })
+        }
+    }, [isMobile, controls])
+
     return (
         <motion.section
             id={id}
-            className="min-h-[calc(100vh-4rem)] flex items-center justify-center"
-            initial={{ opacity: 0, y: 60 }}
+            className={`
+        w-full py-20
+        ${isMobile ? "min-h-screen flex items-center justify-center" : ""}
+      `}
             animate={controls}
             transition={{ duration: 0.7, ease: "easeOut" }}
             viewport={{ amount: 0.3 }}
-
             onViewportEnter={() => {
-                controls.start({ opacity: 1, y: 0 })
+                if (!isMobile) {
+                    controls.start({ opacity: 1, y: 0 })
+                }
                 setActiveSection(id)
             }}
-            onViewportLeave={() =>
-                controls.start({ opacity: 0, y: -60 })
-            }>
+            onViewportLeave={() => {
+                if (!isMobile) {
+                    controls.start({ opacity: 0, y: -60 })
+                }
+            }}
+        >
             {children}
         </motion.section>
-    );
+    )
 }
